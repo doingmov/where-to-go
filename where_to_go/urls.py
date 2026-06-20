9ab46713935b95bd774a4952b54d7cdea3845f5a
doingmov <yohei13111@mail.ru>
2026-06-20 23:16:23 +0300

    Add place details endpoint

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 from django.urls import path
 
 from afisha.views import index
+from places.views import place_details
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('places/<int:place_id>/', place_details),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
