e0da23f93a207d1c082774d5b530ef3318d8476a
doingmov <yohei13111@mail.ru>
2026-06-20 23:18:48 +0300

    Return place details as JSON from API

--- places/views.py
+++ places/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
 from .models import Place
@@ -6,4 +6,22 @@ from .models import Place
 
 def place_details(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
-    return HttpResponse(place.title)
+
+    serialized_place = {
+        'title': place.title,
+        'imgs': [
+            request.build_absolute_uri(image.image.url)
+            for image in place.images.all()
+        ],
+        'description_short': place.short_description,
+        'description_long': place.long_description,
+        'coordinates': {
+            'lat': place.lat,
+            'lng': place.lng,
+        },
+    }
+
+    return JsonResponse(
+        serialized_place,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4},
+    )
